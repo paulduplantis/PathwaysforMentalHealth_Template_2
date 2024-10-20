2265e732c085eb1a3df7d00683b347fc6941305d
J. Paul Duplantis <paulduplantis@gmail.com>
2024-10-19 21:01:53 -0400

    Delete scripts.js
    Signed-off-by: J. Paul Duplantis <paulduplantis@gmail.com>

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Example: Log message to console when the page loads
-    console.log("Deep Inquiry Framework: Page Loaded Successfully");
-
-    // Add more JavaScript code here for dynamic functionality
-});
