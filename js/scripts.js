39b760c4a056fa1fd494bb200a1305cdf12eae68
J. Paul Duplantis <paulduplantis@gmail.com>
2024-10-26 00:22:35 -0400

    Update scripts.js
    Signed-off-by: J. Paul Duplantis <paulduplantis@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,1 @@
 // scripts.js
-
-// Function to toggle expanded content visibility
-function toggleExpand(element) {
-    const card = element.closest('.profile-card, .provider-card, .product-card, .insight-card');
-    if (card) { // Ensure the card element is found
-        card.classList.toggle('expanded'); // Adds or removes the 'expanded' class
-    }
-}
